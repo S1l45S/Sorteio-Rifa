dd92b33f99d17a303dd648ec6b1aeb3124cef83b
Silas Santos <ssilas955@gmail.com>
2026-03-20 03:03:24 -0300

    Ajuste Verificação de DOM

--- FrontEnd/app/layout.jsx
+++ FrontEnd/app/layout.jsx
@@ -3,6 +3,9 @@ import SideBar from '../src/Components/sideBar'
 export const metadata = {
   title: 'Rifa Swarm79',
   description: 'Sorteio da equipe Swarm79',
+  verification: {
+    google: '5QTWFXJrEBDN7hf9bVEd8aJa_owdB1eAhbS2TQCCGmk', 
+  },
 }
 
 export default function RootLayout({ children }) {
